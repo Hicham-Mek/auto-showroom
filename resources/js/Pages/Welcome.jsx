c2526d6626a836514b4c92319310bb14290d838e
Hicham-Mek <mekidichehichem@gmail.com>
2026-08-16 20:18:39 +0100

    Initial commit: Laravel + Inertia + React (JS) + Tailwind + Filament scaffold

---
+++ resources/js/Pages/Welcome.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+const Welcome = () => {
+    return (
+        <div className="p-8">
+            <h1 className="text-3xl font-bold">AutoShowroom</h1>
+            <p>Inertia + React is working.</p>
+        </div>
+    );
+};
+
+export default Welcome;
